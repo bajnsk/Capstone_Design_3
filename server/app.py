d84da94cb6db30a16e392ce9ef388c4e0abf0776
newfull5 <dhtocks@naver.com>
2024-05-05 16:52:08 +0900

    Add script file /server/image_proc.py

--- server/app.py
+++ server/app.py
@@ -12,13 +12,13 @@ app = Flask(__name__)
 # CORS 설정
 CORS(app, resources={r"/upload": {"origins": "http://localhost:3000"}})
 # 확장자 정의
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
 
 # 이미지 업로드 경로 설정
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-UPLOAD_FOLDER = os.path.join(BASE_DIR, 'Upload')
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+UPLOAD_FOLDER = os.path.join(BASE_DIR, "Upload")
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 # import translating model (NLLB-200)
 model = Model()
@@ -32,47 +32,52 @@ model = Model()
 #
 ##############################
 
+
 # 허용할 파일 형식 확인
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 # 이미지에서 텍스트를 추출
 def ocr_core(filename):
-    reader = easyocr.Reader(['ko', 'en'])
+    reader = easyocr.Reader(["ko", "en"])
     results = reader.readtext(filename)
     # 타입 변경
     ocr_results = [
         {
-            'text': result[1],
-            'confidence': float(result[2]),
-            'box': [[int(point[0]), int(point[1])] for point in result[0]]
+            "text": result[1],
+            "confidence": float(result[2]),
+            "box": [[int(point[0]), int(point[1])] for point in result[0]],
         }
         for result in results
     ]
     return ocr_results
 
-@app.route('/upload', methods=['POST'])
+
+@app.route("/upload", methods=["POST"])
 def file_upload():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"}), 400
+
+    file = request.files["file"]
+    if file.filename == "":
+        return jsonify({"error": "No selected file"}), 400
+    
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
         file.save(filepath)
 
         # OCR 적용
         ocr_result = ocr_core(filepath)
-        
+
         # 결과 반환
-        return jsonify({'message': 'File successfully uploaded', 'ocr_result': ocr_result})
+        return jsonify(
+            {"message": "File successfully uploaded", "ocr_result": ocr_result}
+        )
     else:
-        return jsonify({'error': 'Invalid file type'}), 400
+        return jsonify({"error": "Invalid file type"}), 400
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
---
+++ server/image_proc.py
@@ -0,0 +1,59 @@
+import easyocr
+from sentence_splitter import SentenceSplitter
+
+
+class ImageProcessor:
+    def __init__(self, lang="en"):
+        self.reader = easyocr.Reader([lang])
+        self.splitter = SentenceSplitter(language=lang)
+
+    def run_ocr(self, image):
+        """
+        run easy ocr and extract language and location
+
+        :param image: image_path
+        :return: coordinate, English text, probability
+        [
+            (
+                [
+                    [448, 54], [572, 54], [572, 86], [448, 86]
+                ],
+                'Abstract',
+                0.9999912334395931
+            ),
+            ...
+        ]
+        """
+        result = self.reader.readtext(image)
+        return result
+
+    @staticmethod
+    def combine_text(read_text):
+        """
+        detected text is row-by-row which is not a complete sentence.
+        it may reduce translating quality.
+        so, it needs to split sentence by sentence.
+        before split that, combine all together
+
+        :param read_text: only allow for return of run_ocr
+        :return: total_sentence
+        """
+        total_text = ""
+
+        for element in read_text:
+            coordinate, text, prob = element
+            total_text += text
+
+        return total_text
+
+    def sentence_split(self, total_text):
+        """
+        split dummy text with sentence spliter
+
+        :param total_text: return of _combin_text func
+        :return: sentences
+        """
+
+        sentences = self.splitter.split(text=total_text)
+
+        return sentences
