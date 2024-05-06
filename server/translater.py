307522fc0ed46c7a877935c1493487c5e0f6e900
newfull5 <dhtocks@naver.com>
2024-05-06 19:26:30 +0900

    implement image_translate api

--- server/translater.py
+++ server/translater.py
@@ -8,12 +8,21 @@ class Model:
             num_beams=5,
             device='cpu'
     ):
+        """
+
+        :param model_name: huggingface model name
+        :param num_beams: beam search N (more n, more running time)
+        :param device: you can select device 'cpu' or 'cuda'
+        """
+
         self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name, forced_bos_token_id=256098)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name, src_lang='eng_Latn', tgt_lang='kor_Hang')
         self.num_beams = num_beams
+        self.device = device
+        self.model.to(device)
 
     def gen(self, inputs):
-        inputs = self.tokenizer(inputs, return_tensors="pt")
+        inputs = self.tokenizer(inputs, return_tensors="pt").to(self.device)
         output = self.model.generate(
             **inputs,
             # num_beams=self.num_beams
